5bcf43fe9c2f671fcdf6256156e0dc102760db5f
Rebecca Taylor <bex@beccasaur.us>
2020-08-11 16:18:52 -0700

    Working uwsgi script (when CGI plugin present)

--- adapters/uwsgi.sh
+++ adapters/uwsgi.sh
@@ -1,1 +1,5 @@
 # TODO
+#
+# uwsgi --master --processes 1 --plugins http,cgi --http :8080 --http-modifier1 9 --cgi helloWorld.cgi
+#
+# In addition to checking for uwsgi binary, needs to check for cgi plugin
